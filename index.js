37310bc62e1eb61a902fc20ab031fd72ab194fc7
Night Sky <101979092+NightSky13000@users.noreply.github.com>
2023-11-28 18:56:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const config = require('./config.json');
 
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 
-const URL_PANEL = config.panel_url
+const URL_PANEL = config.panel_url;
 const API_KEY = config.apikey;
 
-client.on('ready', async () => {
-  console.log(`${client.user.tag}! Bot Online `);
-  console.log(`Created by NightSky `);
-  console.log(`https://github.com/NightSky13000`);
-
+async function UpdateActivity() {
   try {
     const response = await axios.get(`${URL_PANEL}/api/application/servers`, {
       headers: {
@@ -20,14 +16,22 @@ client.on('ready', async () => {
       },
     });
 
-    const ServerCount = response.data.meta.pagination.total;
+    const serverCount = response.data.meta.pagination.total;
 
     // Activity
-    client.user.setActivity(`${ServerCount} Active Server`, { type: 'WATCHING' });
+    client.user.setActivity(`${serverCount} Active Server`, { type: 'WATCHING' });
     client.user.setStatus('dnd');
   } catch (error) {
     console.error('Error fetching data from Pterodactyl API:', error.message);
   }
+}
+
+client.on('ready', () => {
+  console.log(`${client.user.tag}! Bot Online `);
+  console.log(`Created by NightSky `);
+  console.log(`https://github.com/NightSky13000`);
+
+  setInterval(UpdateActivity, 300000);
 });
 
 // Bot Token
