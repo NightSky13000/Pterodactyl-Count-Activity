355c225af280c24093019eac22f25b9c46488371
Night Sky <101979092+NightSky13000@users.noreply.github.com>
2023-11-28 18:33:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
 const Discord = require('discord.js');
-const mysql = require('mysql');
+const axios = require('axios');
+const config = require('./config.json');
 
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
-// Database Panel
-const dbConnection = mysql.createConnection({
-  host: '192.168.0.1',
-    user: 'ns',
-    password: '123',
-    database: 'panel'
-});
 
-dbConnection.connect((err) => {
-  if (err) {
-    console.error('Database connection error:', err);
-    return;
-  }
-  console.log('Connect to databases.');
-});
+const URL_PANEL = config.url
+const API_KEY = config.api;
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`${client.user.tag}! Bot Online `);
-  
-  dbConnection.query('SELECT COUNT(*) AS serverCount FROM servers', (err, results) => {
-    if (err) {
-      console.error('error database:', err);
-      return;
-    }
-    
-     // Activity
-    const serverCount = results[0].serverCount;
-    client.user.setActivity(`${serverCount} Active Server`, { type: 'WATCHING' });
+  console.log(`Created by NightSky `);
+  console.log(`https://github.com/NightSky13000`);
+
+  try {
+    const response = await axios.get(`${URL_PANEL}/api/application/servers`, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const ServerCount = response.data.meta.pagination.total;
+
+    // Activity
+    client.user.setActivity(`${ServerCount} Active Server`, { type: 'WATCHING' });
     client.user.setStatus('dnd');
-  });
+  } catch (error) {
+    console.error('Error fetching data from Pterodactyl API:', error.message);
+  }
 });
 
 // Bot Token
-client.login('token'); 
+client.login(config.token);
