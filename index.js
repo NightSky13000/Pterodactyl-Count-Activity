51c51103b29bee5e5c8220a21e842a360b7e6c27
Night Sky <101979092+NightSky13000@users.noreply.github.com>
2023-11-28 19:44:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const config = require('./config.json');
 
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 
-const URL_PANEL = config.panel_url
+const URL_PANEL = config.panel_url;
 const API_KEY = config.apikey;
 
-client.on('ready', async () => {
-  console.log(`${client.user.tag}! Bot Online `);
-
+async function UpdateActivity() {
   try {
     const response = await axios.get(`${URL_PANEL}/api/application/servers`, {
       headers: {
@@ -21,12 +19,17 @@ client.on('ready', async () => {
     const ServerCount = response.data.meta.pagination.total;
 
     // Activity
-    client.user.setActivity(`${ServerCount} Active Server`, { type: 'WATCHING' });
+    client.user.setActivity(`${ServerCount} Active Servers`, { type: 'WATCHING' });
     client.user.setStatus('dnd');
   } catch (error) {
     console.error('Error fetching data from Pterodactyl API:', error.message);
   }
+}
+
+client.on('ready', () => {
+  console.log(`${client.user.tag}! Bot Online `);
+
+  setInterval(UpdateActivity, 30 * 1000);
 });
 
-// Bot Token
 client.login(config.token);
