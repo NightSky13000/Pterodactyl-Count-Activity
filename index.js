bb9b31e0e23e676f7d7632b222d2c8f03bd13adb
Night Sky <101979092+NightSky13000@users.noreply.github.com>
2024-05-15 16:32:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,46 @@ const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 const URL_PANEL = config.panel_url;
 const API_KEY = config.apikey;
 
+let ActivityIndex = 0;
+
 async function UpdateActivity() {
   try {
-    const response = await axios.get(`${URL_PANEL}/api/application/servers`, {
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
-        'Content-Type': 'application/json',
-      },
-    });
+    const [ServersResponse, NodesResponse, UsersResponse] = await Promise.all([
+      axios.get(`${URL_PANEL}/api/application/servers`, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      }),
+      axios.get(`${URL_PANEL}/api/application/nodes`, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      }),
+      axios.get(`${URL_PANEL}/api/application/users`, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json',
+        },
+      }),
+    ]);
 
-    const ServerCount = response.data.meta.pagination.total;
+    const ServerCount = ServersResponse.data.meta.pagination.total;
+    const NodeCount = NodesResponse.data.meta.pagination.total;
+    const UserCount = UsersResponse.data.meta.pagination.total;
 
-    // Activity
-    client.user.setActivity(`${ServerCount} Active Servers`, { type: 'WATCHING' });
+    // Activity List
+    const activities = [
+      { type: 'WATCHING', text: `${ServerCount} Servers` },
+      { type: 'WATCHING', text: `${NodeCount} Nodes` },
+      { type: 'WATCHING', text: `${UserCount} Users` },
+    ];
+
+    client.user.setActivity(activities[ActivityIndex].text, { type: activities[ActivityIndex].type });
     client.user.setStatus('dnd');
+
+    ActivityIndex = (ActivityIndex + 1) % activities.length;
   } catch (error) {
     console.error('Error fetching data from Pterodactyl API:', error.message);
   }
@@ -28,7 +54,7 @@ async function UpdateActivity() {
 
 client.on('ready', () => {
   console.log(`${client.user.tag}! Bot Online `);
-
+  UpdateActivity();
   setInterval(UpdateActivity, 30 * 1000);
 });
 
